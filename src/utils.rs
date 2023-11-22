b7451349c33edb91d92dbb8c2f12f67dc009c801
448 OG <superuser@448.africa>
2023-11-22 13:11:40 +0300

    chore(lib): Implement fixes recommended by clippy

--- src/utils.rs
+++ src/utils.rs
@@ -12,11 +12,7 @@ pub struct FsUtils;
 impl FsUtils {
     /// Returns [Option::None] if time query is not supported
     pub fn maybe_time(time_result: Option<SystemTime>) -> Option<Tai64N> {
-        if let Some(time) = time_result {
-            Some(Tai64N::from_system_time(&time))
-        } else {
-            Option::None
-        }
+        time_result.map(|time| Tai64N::from_system_time(&time))
     }
 
     /// Calculate the size in bytes
@@ -54,36 +50,27 @@ impl FsUtils {
 
     /// Convert duration since UNIX EPOCH to humantime
     pub fn tai64_to_humantime_with_epoch(time: &Tai64N) -> Option<String> {
-        if let Some(duration) = FsUtils::tai64_duration_since_epoch(time) {
-            Some(humantime::format_duration(duration).to_string())
-        } else {
-            None
-        }
+        FsUtils::tai64_duration_since_epoch(time)
+            .map(|duration| humantime::format_duration(duration).to_string())
     }
 
     /// Convert duration since two TAI64N timestamps to humantime
     pub fn tai64_to_humantime(earlier_time: &Tai64N, current_time: &Tai64N) -> Option<String> {
-        if let Some(duration) = FsUtils::tai64_duration(earlier_time, current_time) {
-            Some(humantime::format_duration(duration).to_string())
-        } else {
-            None
-        }
+        FsUtils::tai64_duration(earlier_time, current_time)
+            .map(|duration| humantime::format_duration(duration).to_string())
     }
 
     /// Convert duration between current time and earlier TAI64N timestamp to humantime
     pub fn tai64_now_duration_to_humantime(earlier_time: &Tai64N) -> Option<String> {
-        if let Some(duration) = FsUtils::tai64_duration_from_now(earlier_time) {
-            Some(humantime::format_duration(duration).to_string())
-        } else {
-            None
-        }
+        FsUtils::tai64_duration_from_now(earlier_time)
+            .map(|duration| humantime::format_duration(duration).to_string())
     }
 
     /// Get the duration between two TAI64N timestamps
     pub fn tai64_duration(earlier_time: &Tai64N, current_time: &Tai64N) -> Option<Duration> {
-        match earlier_time.duration_since(&Tai64N::UNIX_EPOCH) {
+        match earlier_time.duration_since(current_time) {
             Ok(valid_time) => Some(valid_time),
-            Err(_) => return Option::None,
+            Err(_) => Option::None,
         }
     }
 
@@ -91,15 +78,15 @@ impl FsUtils {
     pub fn tai64_duration_since_epoch(time: &Tai64N) -> Option<Duration> {
         match time.duration_since(&Tai64N::UNIX_EPOCH) {
             Ok(valid_time) => Some(valid_time),
-            Err(_) => return Option::None,
+            Err(_) => Option::None,
         }
     }
 
     /// Get the duration since UNIX EPOCH
     pub fn tai64_duration_from_now(earlier_time: &Tai64N) -> Option<Duration> {
-        match Tai64N::now().duration_since(&earlier_time) {
+        match Tai64N::now().duration_since(earlier_time) {
             Ok(valid_time) => Some(valid_time),
-            Err(_) => return Option::None,
+            Err(_) => Option::None,
         }
     }
 }
