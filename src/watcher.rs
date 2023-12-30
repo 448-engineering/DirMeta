0391edb7fe9371f7a4db4676aeb166150159656c
448 OG <superuser@448.africa>
2023-12-30 18:23:23 +0300

    fix(docs): Fix missing crate imports
    chore(docs): Implement cargo fmt recommendations

--- src/watcher.rs
+++ src/watcher.rs
@@ -15,31 +15,32 @@ pub type FsSender = Sender<WatcherOutcome>;
 ///
 /// #### Structure
 /// ```rust
+/// use dir_meta::FsSender;
+/// use std::path::PathBuf;
+///
 /// #[derive(Debug)]
 /// pub struct FsWatcher {
 ///     path: Option<PathBuf>,
-///     sender: FsSender
+///     sender: FsSender,
 /// }
-///
 /// ```
 ///
 /// #### Example
 /// ```rust
 /// use dir_meta::{inotify::WatchMask, smol::channel, FsWatcher, WatcherOutcome};
 ///
-///    smol::block_on(async {
-///         let (sender, receiver) = channel::unbounded::<WatcherOutcome>();
+/// smol::block_on(async {
+///     let (sender, receiver) = channel::unbounded::<WatcherOutcome>();
 ///
-///        let watch_options =
-///            WatchMask::MODIFY | WatchMask::CREATE | WatchMask::DELETE | WatchMask::DELETE_SELF;
+///     let watch_options =
+///         WatchMask::MODIFY | WatchMask::CREATE | WatchMask::DELETE | WatchMask::DELETE_SELF;
 ///
-///        smol::spawn(FsWatcher::new(sender).path("Foo").watch(watch_options)).detach();
+///     smol::spawn(FsWatcher::new(sender).path("Foo").watch(watch_options)).detach();
 ///
-///        while let Ok(data) = receiver.recv().await {
-///            dbg!(data);
-///        }
-///    });
-///
+///     while let Ok(data) = receiver.recv().await {
+///         dbg!(data);
+///     }
+/// });
 /// ```
 #[derive(Debug)]
 pub struct FsWatcher {
